7de9485b40fb38b54f5fd9c4a8b995abc08919e4
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-03-07 15:05:16 +0900

    viewport

--- deploy-pdf.ts
+++ deploy-pdf.ts
@@ -2,10 +2,15 @@ import { serve } from 'https://deno.land/std@0.114.0/http/server.ts';
 import { demoPdf } from './demo-pdf.ts'
 
 const html = `
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
 <form method="POST" action="/">
   <input type="text" name="text" placeholder="何か入力して下さい">
   <button type="submit">Submit</button>
 </form>
+</body>
 `;
 
 console.log('Listening on http://localhost:8000');
